ac46a1c95f40d0b8850b542741fbe63ee34f735c
Abhinav <32677073+abiydv@users.noreply.github.com>
2019-01-21 15:09:11 +0530

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,42 +1,42 @@
 provider "aws" {
     shared_credentials_file = "$HOME/.aws/credentials"
-    profile                 = "default"
+    profile                 = "${var.aws_profile}"
     region                  = "${var.aws_region}"
 }
 
 module "backup_lambda" {
-   source              = "${path.module}../modules/lambda"
+   source              = "${path.module}/../modules/lambda"
    function_name       = "${var.environment}_backup_lambda_function"
    description         = "Takes snapshots of ${var.role} volumes"
-   filename            = "${path.module}../code/${var.environment}/${var.environment}_backup_lambda_function.zip"
-   handler             = "${var.environment}_backup_lambda_function.lambda_handler"
+   filename            = "${path.module}/../code/${var.environment}-ebs-snapshot-backup.zip"
+   handler             = "${var.environment}-ebs-snapshot-backup.lambda_handler"
    role_arn            = "${module.lambda_role.role_arn}"
    trigger_service     = "events.amazonaws.com"
    trigger_service_arn = "${module.backup_event.event_arn}"
 }
 
 module "cleanup_lambda" {
-   source              = "${path.module}../modules/lambda"
+   source              = "${path.module}/../modules/lambda"
    function_name       = "${var.environment}_cleanup_lambda_function"
    description         = "Removes ${var.retention} days old snapshot(s) of ${var.role} volumes"
-   filename            = "${path.module}../code/${var.environment}/${var.environment}_cleanup_lambda_function.zip"
-   handler             = "${var.environment}_cleanup_lambda_function.lambda_handler"
+   filename            = "${path.module}/../code/${var.environment}-ebs-snapshot-cleanup.zip"
+   handler             = "${var.environment}-ebs-snapshot-cleanup.lambda_handler"
    role_arn            = "${module.lambda_role.role_arn}"
    trigger_service     = "events.amazonaws.com"
    trigger_service_arn = "${module.cleanup_event.event_arn}"
 }
 
 module "lambda_role" {
-   source            = "${path.module}../modules/role"
-   role_name         = "${var.environment}_lambda_role"
-   policy_name       = "${var.environment}_lambda_role_policy"
-   topic_arn         = "${module.sns.topic_arn}"
-   role_policy       = "role-policy.json"
+   source             = "${path.module}/../modules/role"
+   role_name          = "${var.environment}_lambda_role"
+   policy_name        = "${var.environment}_lambda_role_policy"
+   topic_arn          = "${module.sns.topic_arn}"
+   role_policy        = "role-policy.json"
    assume_role_policy = "assume-role-policy.json"
 }
 
 module "backup_event" {
-   source            = "${path.module}../modules/event"
+   source            = "${path.module}/../modules/event"
    rule_name         = "${var.environment}_backup_event"
    target_arn        = "${module.backup_lambda.lambda_arn}"
    topic_arn         = "${module.sns.topic_arn}"
@@ -47,7 +47,7 @@ module "backup_event" {
 }
 
 module "cleanup_event" {
-   source            = "${path.module}../modules/event"
+   source            = "${path.module}/../modules/event"
    rule_name         = "${var.environment}_cleanup_event"
    target_arn        = "${module.cleanup_lambda.lambda_arn}"
    target_input_json = "event-input.json"
@@ -58,7 +58,7 @@ module "cleanup_event" {
 }
 
 module "sns" {
-   source          = "${path.module}../modules/sns"
+   source          = "${path.module}/../modules/sns"
    topic_name      = "${var.environment}_notify_team"
    team_email      = "${var.team_email}"
    aws_profile     = "${var.aws_profile}"
